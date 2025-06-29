1d5fe52638fa47ebc89683b593eb912894ce329e
HellPortal <104918285+HellPortal@users.noreply.github.com>
2025-06-29 16:22:24 -0400

    Create fxmanifest.lua

---
+++ delperro_clean/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'HellPotal'
+description 'Cleaned Del Perro Pier map for FiveM - ready to be used in custom projects'
+version '1.0.0'
+
+this_is_a_map 'yes'
